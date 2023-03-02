b0609e16009f2c2c7782316f1f3930e85eceb9de
yexuehua <yxh1164691705@qq.com>
2023-03-02 23:16:09 +0800

    add decision tree

---
+++ DT.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+
+from collections import Counter
+import math
+from math import log
+
+import pprint
+
+# 书上题目5.1
+def create_data():
+    datasets = [['青年', '否', '否', '一般', '否'],
+               ['青年', '否', '否', '好', '否'],
+               ['青年', '是', '否', '好', '是'],
+               ['青年', '是', '是', '一般', '是'],
+               ['青年', '否', '否', '一般', '否'],
+               ['中年', '否', '否', '一般', '否'],
+               ['中年', '否', '否', '好', '否'],
+               ['中年', '是', '是', '好', '是'],
+               ['中年', '否', '是', '非常好', '是'],
+               ['中年', '否', '是', '非常好', '是'],
+               ['老年', '否', '是', '非常好', '是'],
+               ['老年', '否', '是', '好', '是'],
+               ['老年', '是', '否', '好', '是'],
+               ['老年', '是', '否', '非常好', '是'],
+               ['老年', '否', '否', '一般', '否'],
+               ]
+    labels = [u'年龄', u'有工作', u'有自己的房子', u'信贷情况', u'类别']
+    # 返回数据集和每个维度的名称
+    return datasets, labels
+
+
+datasets, labels = create_data()
+train_data = pd.DataFrame(datasets, columns=labels)
